237d07cdb3b97a80f34f6b7bdffe0cdf478cbdbd
WilliamQuinones <daniel@sebrene.com>
2016-04-11 22:25:50 -0400

    Upload functionality complete(mostly)
    Camera no longer crashes app, can now upload pictures taken from app.
    Added upload button restrictions, all fields must be filled and image
    has to be selected.
    Image uploaded now associated with corresponding tags in database.

--- app/src/main/java/com/ivchen/flyershare/CreateActivity.java
+++ app/src/main/java/com/ivchen/flyershare/CreateActivity.java
@@ -14,8 +14,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.util.Base64;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -26,11 +28,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateActivity extends AppCompatActivity {
     int REQUEST_CAMERA = 0; //needs to be initialized for onClickButtonCamera method to work
     private static int SELECT_FILE = 1; //needs to be initialized for startActivityForResult method to work
     ImageView image;
+    EditText title, date, school;
 
     Uri selectedImage;
 
@@ -40,6 +45,9 @@ public class CreateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create);
         Firebase.setAndroidContext(this);
         image = (ImageView) findViewById(R.id.imageView2);
+        title = (EditText) findViewById(R.id.editText);
+        date = (EditText) findViewById(R.id.editText2);
+        school = (EditText) findViewById(R.id.editText3);
 
     }
 
@@ -71,16 +79,41 @@ public class CreateActivity extends AppCompatActivity {
     public void onClickbuttonUpload(View view) {
 
 
-            Bitmap bmp = ((BitmapDrawable) image.getDrawable()).getBitmap();
-            ByteArrayOutputStream bYtE = new ByteArrayOutputStream();
-            bmp.compress(Bitmap.CompressFormat.JPEG, 100, bYtE);
-            bmp.recycle();
-            byte[] byteArray = bYtE.toByteArray();
-            String imageFile = Base64.encodeToString(byteArray, Base64.DEFAULT);
+       try{
 
-            Firebase ref = new Firebase("https://flyershare.firebaseio.com");
-            ref.child("flyershare").setValue(imageFile);
-            Toast.makeText(CreateActivity.this, "Uploading . . .", Toast.LENGTH_SHORT).show();
+           String editTitle = title.getText().toString();
+           String editDate = date.getText().toString();
+           String editSchool = school.getText().toString();
+           if(editTitle.trim().length() > 0 && editDate.trim().length() > 0 && editSchool.trim().length() > 0) {
+               Bitmap bmp = ((BitmapDrawable) image.getDrawable()).getBitmap();
+               ByteArrayOutputStream bYtE = new ByteArrayOutputStream();
+               bmp.compress(Bitmap.CompressFormat.JPEG, 100, bYtE);
+               bmp.recycle();
+               byte[] byteArray = bYtE.toByteArray();
+               String imageFile = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+               Firebase ref = new Firebase("https://flyershare.firebaseio.com");
+               Firebase postRef = ref.child("posts");
+               Map<String, String> flyer = new HashMap<String, String>();
+               flyer.put("image", imageFile);
+               flyer.put("date", editDate);
+               flyer.put("title", editTitle);
+               flyer.put("school", editSchool);
+               postRef.push().setValue(flyer);
+
+               Toast.makeText(CreateActivity.this, "Uploading . . .", Toast.LENGTH_SHORT).show();
+           }else{
+               Toast.makeText(CreateActivity.this, "Make sure all fiels are completed!", Toast.LENGTH_SHORT).show();
+           }
+       }catch(Exception e){
+           Toast.makeText(CreateActivity.this, "Make sure image is selected!", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+
+
+
 
 
     }
@@ -94,24 +127,9 @@ public class CreateActivity extends AppCompatActivity {
                 selectedImage = data.getData();
                 image.setImageURI(selectedImage);
 
-            } else if(requestCode == REQUEST_CAMERA){
+            } else if(requestCode == REQUEST_CAMERA && resultCode == RESULT_OK){
                 //camera crashes after picture is taken!!
                 Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-                thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
-                File destination = new File(Environment.getExternalStorageDirectory(),
-                        System.currentTimeMillis() + ".jpg");
-                FileOutputStream fo;
-                try {
-                    destination.createNewFile();
-                    fo = new FileOutputStream(destination);
-                    fo.write(bytes.toByteArray());
-                    fo.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
                 image.setImageBitmap(thumbnail);
             }
     }
