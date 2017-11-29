213cbc6275e0a58df7ed3b751b2c9fed9ec48511
Lawrence Chen <the_Crimson_chen@yahoo.com>
2017-11-29 18:22:46 -0500

    added necessary files for commit

--- app/src/main/java/com/ivchen/flyershare/MainActivity.java
+++ app/src/main/java/com/ivchen/flyershare/MainActivity.java
@@ -6,11 +6,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import com.ivchen.flyershare.R;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+
     }
 
     @Override
@@ -43,10 +53,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickLogin(View view) {
-        Intent intent = new Intent(MainActivity.this, MainBoardActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
 
+        Firebase ref = new Firebase("https://flyershare.firebaseio.com");
+
+        //log in info hardcoded in, delete these two lines and uncomment the others to allow
+        //actual authentication
+        String userInput = "flyershareofficial@gmail.com";
+        String passwordInput = "apps4998";
+        /*EditText txtDescription =
+                (EditText) findViewById(R.id.usernameField);
+        String userInput = txtDescription.getText().toString();
+        txtDescription =
+                (EditText) findViewById(R.id.passwordField);
+        String passwordInput = txtDescription.getText().toString();*/
+        ref.authWithPassword(userInput, passwordInput, new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
+                Intent intent = new Intent(MainActivity.this, MainBoardActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                Toast.makeText(getApplicationContext(), "Error with user credentials.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void onClickSignUp(View view) {
